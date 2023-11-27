293da107b5e70aeb815078f717ade4fdcc620c50
stsabouz <salah-eddine.bouzerda@stud.h-da.de>
2023-11-27 17:38:09 +0100

    Refactor User model schema

--- models/User.js
+++ models/User.js
@@ -1,24 +1,44 @@
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const {verify} = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const {Schema, model} = mongoose;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    group: {
-        type: String,
-        enum: ['Admin', 'Moderator', 'Student'],
-        default: 'Student'
-    }
+  firstName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 25,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 25,
+  },
+  matrikelNr: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  group: {
+    type: String,
+    enum: ["Admin", "Moderator", "Student"],
+    default: "Student",
+  },
+  verifyed: {
+    type: Boolean,
+    default: "false",
+  },
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
